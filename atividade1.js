5726bedd042551bef132e4aeb371fa5d28e2ddfc
nella <141764262+coccinellaa@users.noreply.github.com>
2024-09-24 17:43:04 -0400

    Add files via upload

---
+++ atividade1.js
@@ -0,0 +1,63 @@
+// 1 - Classe
+class Celular { // classe Celular
+    constructor(marca, cor, lancamento) { 
+    this.marca = marca; 
+    this.cor = cor;
+    this.lancamento = lancamento;
+    }
+}
+// -----------------------------------------------------------
+// 2 - Objeto
+let meuCelular = new Object();
+meuCelular.marca = "Apple";
+meuCelular.cor = "Grafite";
+meuCelular.lancamento = 2021;
+
+// -----------------------------------------------------------
+// 3 - Atributos
+class Celular {
+    constructor(marca, cor, lancamento) { 
+    this.marca = marca; // atributo referente à classe Celular
+    this.cor = cor; // atributo referente à classe Celular
+    this.lancamento = lancamento; // atributo referente à classe Celular
+    }
+}
+
+// -----------------------------------------------------------
+// 4 - Método
+let agua = {
+    subs: 'h2o',
+    substancia: function(h2, o) {
+        console.log(agua.subs);
+    }
+}
+
+agua.substancia();
+
+// -----------------------------------------------------------
+// 5 - Herança
+function geracaoZ (contexto, comportamento, caracteristicas) {
+    this.contexto = {
+        first,
+        last,
+      };
+    this.comportamento = comportamento;
+    this.caracteristicas = caracteristicas;
+  }
+
+// -----------------------------------------------------------
+// 6 - Polimorfismo
+
+export class geracaoZ extends geracao {
+    public decimal{
+        CalcularVendas()
+        {
+        decimal valorUnitario =
+        decimal.MinValue;
+        decimal produtosVendidos =
+        decimal.MinValue;
+        return valorUnitario *
+        produtosVendidos; } 
+    }
+
+}
